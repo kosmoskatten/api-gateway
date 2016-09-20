e6a42cf2e225d5f9212172d83dd1086b03f2eb87
Patrik Sandahl <patrik.sandahl@ericsson.com>
2016-09-20 13:32:33 +0200

    Create MME pco.

--- api-gateway/src/Api/MmeV1.hs
+++ api-gateway/src/Api/MmeV1.hs
@@ -23,6 +23,7 @@ module Api.MmeV1
     ) where
 
 import Data.Aeson (FromJSON, ToJSON)
+import Data.String.Conversions (cs)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import Network.Nats
@@ -39,6 +40,10 @@ data UrlRef = UrlRef
     { url :: !Text
     } deriving (Generic, Show, FromJSON, ToJSON)
 
+data CreateStatus = CreateStatus
+    { status :: !Int
+    } deriving (Generic, Show, FromJSON, ToJSON)
+
 -- | API for listing all registered Mmes.
 type ListMmes = "api" :> "v1" :> "mme" :> Get '[JSON] [UrlRef]
 
@@ -50,17 +55,25 @@ listMmes :: Self -> Handler [UrlRef]
 listMmes Self {..} =
     tmoRequest tmo (request nats "app.v1.mme.listPcos" "") $ \msg ->
         maybe (throwError err502) (return . map toUrlRef) (jsonPayload msg)
-    where
-        toUrlRef :: Text -> UrlRef
-        toUrlRef mmeName =
-            UrlRef { url = "/api/v1/mme/" `mappend` mmeName }
 
 type CreateMme = "api" :> "v1" :> "mme"
                        :> ReqBody '[JSON] NameRef
                        :> PostCreated '[JSON] UrlRef
 
+-- | Request the app.v1.mme.createPco service to create a new pco with
+-- the given name.
 createMme :: Self -> NameRef -> Handler UrlRef
-createMme = undefined
+createMme Self {..} NameRef {..} = do
+    let topic' = "app.v1.mme.createPco." `mappend` (cs name)
+    tmoRequest tmo (request nats topic' "") $ \msg ->
+        maybe (throwError err502) handleStatus (jsonPayload msg)
+    where
+        handleStatus :: CreateStatus -> Handler UrlRef
+        handleStatus CreateStatus {..} =
+            case status of
+                201 -> return $ toUrlRef name
+                409 -> throwError err409
+                _   -> throwError err502
 
 type DeleteMme = "api" :> "v1" :> "mme" :> Capture "name" Text
                        :> DeleteNoContent '[JSON] NoContent
@@ -73,3 +86,7 @@ type GetIpConfig = "api" :> "v1" :> "mme" :> Capture "name" Text
 
 getIpConfig :: Self -> Text -> Handler [Text]
 getIpConfig = undefined
+
+toUrlRef :: Text -> UrlRef
+toUrlRef mmeName =
+    UrlRef { url = "/api/v1/mme/" `mappend` mmeName }
